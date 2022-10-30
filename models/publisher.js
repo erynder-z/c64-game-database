3dcb99ef7033284f538c95ba09470545fbe43c15
erynder-z <stfn.bgr@gmail.com>
2022-10-30 20:27:53 +0100

    Add publisher model

---
+++ models/publisher.js
@@ -0,0 +1,18 @@
+const { DateTime } = require('luxon');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const PublisherSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  founded: { type: Number },
+  defunct: { type: Number },
+});
+
+// Virtual for author's URL
+PublisherSchema.virtual('url').get(function () {
+  return `/library/publisher/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model('Publisher', PublisherSchema);
