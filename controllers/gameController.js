1f9e5d21653676b72aab13c5b46cfe63017a14d4
erynder-z <stfn.bgr@gmail.com>
2022-11-03 22:12:40 +0100

    Add game create controller

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,3 +1,4 @@
+const { body, validationResult } = require('express-validator');
 const Game = require('../models/game');
 const Publisher = require('../models/publisher');
 const Genre = require('../models/genre');
@@ -16,12 +17,22 @@ exports.index = (req, res) => {
       genre_count(callback) {
         Genre.countDocuments({}, callback);
       },
+      publishers(callback) {
+        Publisher.find(callback);
+      },
+      genres(callback) {
+        Genre.find(callback);
+      },
     },
     (err, results) => {
       res.render('index', {
         title: 'C64 game database',
         error: err,
-        data: results,
+        game_count: results.game_count,
+        publisher_count: results.publisher_count,
+        genre_count: results.genre_count,
+        publishers: results.publishers,
+        genres: results.genres,
       });
     }
   );
@@ -75,14 +86,113 @@ exports.game_detail = (req, res) => {
 };
 
 // Display game create form on GET.
-exports.game_create_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Game create GET');
+exports.game_create_get = (req, res, next) => {
+  async.parallel(
+    {
+      publishers(callback) {
+        Publisher.find(callback);
+      },
+      genres(callback) {
+        Genre.find(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      res.render('add_game_form', {
+        title: 'Add Game',
+        publishers: results.publishers,
+        genres: results.genres,
+      });
+    }
+  );
 };
 
 // Handle game create on POST.
-exports.game_create_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Game create POST');
-};
+exports.game_create_post = [
+  (req, res, next) => {
+    if (!Array.isArray(req.body.genre)) {
+      req.body.genre =
+        typeof req.body.genre === 'undefined' ? [] : [req.body.genre];
+    }
+    next();
+  },
+
+  // Validate and sanitize fields.
+  body('title', 'Title must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('publisher', 'Publisher must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('summary', 'Summary must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('genre.*').escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped and trimmed data.
+    const game = new Game({
+      title: req.body.title,
+      publisher: req.body.publisher,
+      summary: req.body.summary,
+      genre: req.body.genre,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all authors and genres for form.
+      async.parallel(
+        {
+          publishers(callback) {
+            Publisher.find(callback);
+          },
+          genres(callback) {
+            Genre.find(callback);
+          },
+        },
+        (err, results) => {
+          if (err) {
+            return next(err);
+          }
+
+          // Mark our selected genres as checked.
+          for (const genre of results.genres) {
+            if (game.genre.includes(genre._id)) {
+              genre.checked = 'true';
+            }
+          }
+          res.render('add_game_form', {
+            title: 'Add Game',
+            publishers: results.publishers,
+            genres: results.genres,
+            game,
+            errors: errors.array(),
+          });
+        }
+      );
+      return;
+    }
+
+    // Data from form is valid. Save book.
+    game.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful: redirect to new book record.
+      res.redirect(game.url);
+    });
+  },
+];
 
 // Display game delete form on GET.
 exports.game_delete_get = (req, res) => {
