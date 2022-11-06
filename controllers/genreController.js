66e3ec8568718970bcc4f4ea26ff1a307cbc5c17
erynder-z <stfn.bgr@gmail.com>
2022-11-06 21:01:56 +0100

    Add genre delete controller

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -109,7 +109,7 @@ exports.genre_delete_get = (req, res) => {
       genre(callback) {
         Genre.findById(req.params.id).exec(callback);
       },
-      genre_books(callback) {
+      genre_games(callback) {
         Game.find({ genre: req.params.id }).exec(callback);
       },
     },
@@ -170,10 +170,70 @@ exports.genre_delete_post = (req, res) => {
 
 // Display Genre update form on GET.
 exports.genre_update_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Genre update GET');
+  // Get game, publishers and genres for form.
+  async.parallel(
+    {
+      genre(callback) {
+        Genre.findById(req.params.id).exec(callback);
+      },
+      genre_games(callback) {
+        Game.find({ genre: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.genre == null) {
+        // No results.
+        const err = new Error('Genre not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Success.
+      res.render('add_genre_form', {
+        title: 'Update Genre',
+        genre: results.genre,
+        genre_games: results.genre_games,
+      });
+    }
+  );
 };
 
 // Handle Genre update on POST.
-exports.genre_update_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+  // Validate and sanitize the name field.
+  body('genreName', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const genre = new Genre({
+      name: req.body.genreName,
+      _id: req.params.id, //This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values and error messages.
+      res.render('add_genre_form', {
+        title: 'Update Genre',
+        genre: genre,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    // Data from form is valid. Update the record.
+    Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, theGenre) => {
+      if (err) {
+        return next(err);
+      }
+
+      // Successful: redirect to book detail page.
+      res.redirect(theGenre.url);
+    });
+  },
+];
