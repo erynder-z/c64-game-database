c70eb32f4d025445dc29fc319bae9a858f291d26
erynder-z <stfn.bgr@gmail.com>
2022-11-06 20:17:03 +0100

    Add game update controller

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -136,6 +136,7 @@ exports.game_create_post = [
       publisher: req.body.publisher,
       summary: req.body.summary,
       genre: req.body.genre,
+      year: req.body.year,
     });
 
     if (!errors.isEmpty()) {
@@ -223,10 +224,136 @@ exports.game_delete_post = (req, res) => {
 
 // Display game update form on GET.
 exports.game_update_get = (req, res) => {
-  res.send('NOT IMPLEMENTED: Game update GET');
+  // Get game, publisheres and genres for form.
+  async.parallel(
+    {
+      game(callback) {
+        Game.findById(req.params.id)
+          .populate('publisher')
+          .populate('genre')
+          .exec(callback);
+      },
+      publishers(callback) {
+        Publisher.find(callback);
+      },
+      genres(callback) {
+        Genre.find(callback);
+      },
+    },
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.game == null) {
+        // No results.
+        const err = new Error('Game not found');
+        err.status = 404;
+        return next(err);
+      }
+      // Success.
+      // Mark our selected genres as checked.
+      for (const genre of results.genres) {
+        for (const gameGenre of results.game.genre) {
+          if (genre._id.toString() === gameGenre._id.toString()) {
+            genre.checked = 'true';
+          }
+        }
+      }
+      res.render('add_game_form', {
+        title: 'Update Game',
+        publishers: results.publishers,
+        genres: results.genres,
+        game: results.game,
+      });
+    }
+  );
 };
 
 // Handle game update on POST.
-exports.game_update_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Game update POST');
-};
+exports.game_update_post = [
+  // Convert the genre to an array
+  (req, res, next) => {
+    if (!Array.isArray(req.body.genre)) {
+      req.body.genre =
+        typeof req.body.genre === 'undefined' ? [] : [req.body.genre];
+    }
+    next();
+  },
+
+  // Validate and sanitize fields.
+  body('title', 'Title must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('publisher', 'Publisher must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('summary', 'Summary must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('genre.*').escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped/trimmed data and old id.
+    const game = new Game({
+      title: req.body.title,
+      publisher: req.body.publisher,
+      summary: req.body.summary,
+      genre: typeof req.body.genre === 'undefined' ? [] : req.body.genre,
+      year: req.body.year,
+      _id: req.params.id, //This is required, or a new ID will be assigned!
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/error messages.
+
+      // Get all authors and genres for form.
+      async.parallel(
+        {
+          publishers(callback) {
+            Publisher.find(callback);
+          },
+          genres(callback) {
+            Genre.find(callback);
+          },
+        },
+        (err, results) => {
+          if (err) {
+            return next(err);
+          }
+
+          // Mark our selected genres as checked.
+          for (const genre of results.genres) {
+            if (game.genre.includes(genre._id)) {
+              genre.checked = 'true';
+            }
+          }
+          res.render('add_game_form', {
+            title: 'Update Game',
+            publishers: results.publishers,
+            genres: results.genres,
+            game,
+            errors: errors.array(),
+          });
+        }
+      );
+      return;
+    }
+
+    // Data from form is valid. Update the record.
+    Game.findByIdAndUpdate(req.params.id, game, {}, (err, theGame) => {
+      if (err) {
+        return next(err);
+      }
+
+      // Successful: redirect to book detail page.
+      res.redirect(theGame.url);
+    });
+  },
+];
