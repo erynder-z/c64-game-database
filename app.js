0d715cd367732aff99e7a915d0a7271b9197d03c
erynder-z <stfn.bgr@gmail.com>
2022-11-17 22:07:25 +0100

    Prepare for deployment

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const libraryRouter = require('./routes/library');
-/* const compression = require('compression');
-const helmet = require('helmet'); */
+const compression = require('compression');
+const helmet = require('helmet');
 
 const app = express();
 
 const mongoose = require('mongoose');
-/* const dev_db_url = `${env.database_url}`;
-const mongoDB = process.env.MONGODB_URI || dev_db_url; */
-const mongoDB = `${env.database_url}`;
+const dev_db_url = `${env.database_url}`;
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+/* const mongoDB = `${env.database_url}`; */
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -24,13 +24,13 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-/* app.use(helmet()); */
+app.use(helmet());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-/* app.use(compression()); */
+app.use(compression());
 
 app.use('/', indexRouter);
 app.use('/library', libraryRouter);
