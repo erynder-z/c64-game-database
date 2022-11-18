bd1853b0cd8bf712981b3fd93bf5dddbd8e71297
erynder-z <stfn.bgr@gmail.com>
2022-11-18 11:00:16 +0100

    Substitute inline javascript

--- public/javascripts/ui_scripts.js
+++ public/javascripts/ui_scripts.js
@@ -1,19 +1,27 @@
 // Navbar toggle
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.sidenav');
-  var instances = M.Sidenav.init(elems, {});
+  const elems = document.querySelectorAll('.sidenav');
+  const instances = M.Sidenav.init(elems, {});
 });
 
 // Fab
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  var instances = M.FloatingActionButton.init(elems, {
+  const elems = document.querySelectorAll('.fixed-action-btn');
+  const instances = M.FloatingActionButton.init(elems, {
     toolbarEnabled: true,
   });
 });
 
 // Form dropdown
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems, {});
+  const elems = document.querySelectorAll('select');
+  const instances = M.FormSelect.init(elems, {});
+});
+
+// Back button
+document.addEventListener('DOMContentLoaded', function () {
+  const backBtn = document.getElementById('go-back');
+  backBtn?.addEventListener('click', () => {
+    history.back();
+  });
 });
